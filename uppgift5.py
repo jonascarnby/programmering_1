f6cf1655f57d361981b8e6e796160ebc62be17b9
jonascarnby <jonascarnby@gmail.com>
2015-01-14 10:15:23 +0100

    Update

--- uppgift5.py
+++ uppgift5.py
@@ -7,13 +7,14 @@ OSX 10.9.5
 Uppgift 5
 """
 
-#importerar Tkinter
+#importerar Tkinter och koden från tidigare uppgift 
 import sys
 from tkinter import *
 import tkinter.messagebox
 import JonasCarnbyUppgift3py3 as paliCheck
 
-def check():
+#Funktion för att kontrollera Palindromdet 
+def check(to_check=None):
 
     x = str(ment.get())
     x = x.lower()
@@ -22,24 +23,40 @@ def check():
     
 
     if result == True:
-        svarLabel.config(text="Din inmatning är ett Palindrome")
+        svarLabel.config(text="Din inmatning är ett Palindrome",fg="green",font= ("helvetica", 16))
+        spara_knapp.config(state=NORMAL)
 
     else:
-        svarLabel.config(text="Din inmatning är INTE ett Palindrome")
+        svarLabel.config(text="Din inmatning är INTE ett Palindrome",fg="red", font=("helvetica", 16))
 
+#Funktion för att spara ett godkänt result 
 def save(result):
-
-    f = open("Palindromesave", 'w')
-    f.write(result)
+    if not check(result):
+        return
+    f = open("Palindromesave", 'a')
+    f.write(result+"\n")
     f.close()
 
+    #försök till info om sparning
+    svarLabel.config(text="Ditt Palindrome är sparat" ,fg="green")
+
+#Funktion för att avsluta programmet
 def quit():
+
     result = messagebox.askyesno('Avsluta programmet', 'Vill du verkligen avsluta programmet?')
     if result == True:
-        mGui.destroy() 
+        mGui.destroy()
 
+#Funktion för att visa information kopplad till info_knapp 
+def info():
+
+    messagebox.showinfo("Info","I det här programmet kan du kontrollera om ett visst ord är ett Palindrome.\
+ Är ordet ett palindrome har du också möjlighet att spara detta till en separat fil genom att klicka på Spara-knappen ")
+
+    
+#Main GUI
 mGui = Tk()
-mGui.geometry("800x300+300+300")
+mGui.geometry("600x200+300+300")
 topFrame = Frame(mGui)
 topFrame.pack()
 midFrame = Frame(mGui)
@@ -51,24 +68,27 @@ bottomFrame.config(width="300")
 ment = StringVar()
 mGui.title("Palindrome?")
 
+#Knappar och labels i Topframe
 topLabel = Label(topFrame, text = "Skriv in texten du vill kontrollera: ")
 topLabel.pack(side="left")
 uInput = Entry(topFrame, textvariable = ment)
 uInput.pack(side="left")
 kontrollera_knapp = Button (topFrame, text = "Kontrollera", command = check)
 kontrollera_knapp.pack(side="left")
-info_knapp = Button(topFrame, text = "Info")
+info_knapp = Button(topFrame, text = "Info", command = info)
 info_knapp.pack(side="left")
 
+#midFrame
 svarLabel = Label(midFrame, text = "")
 svarLabel.pack()
 
-spara_knapp = Button(bottomFrame, text = "Spara Palindrome", command = save)
-spara_knapp.pack(side="left")
+#bottomFrame
 avsluta_knapp = Button(bottomFrame, text = "Avsluta", command = quit)
-avsluta_knapp.pack(side="right")
+avsluta_knapp.pack(side="bottom")
+spara_knapp = Button(bottomFrame, text = "Spara Palindrome", command = lambda: save(ment.get()))
+spara_knapp.config(state=DISABLED)
+spara_knapp.pack(side="bottom")
+
 
 mGui.mainloop()
    
-if __name__=="__main__":
-    main()
